e871fbf2c292f0be28ee9e427c571a2dffd67e80
Øystein Bringsli <oysteinmb@gmail.com>
2024-01-25 14:42:30 +0100

    Fixed RGB Struct import

--- include/LedController.hpp
+++ include/LedController.hpp
@@ -5,14 +5,15 @@
 #ifndef LEDSTRIPCONTROLLER_LEDCONTROLLER_HPP
 #define LEDSTRIPCONTROLLER_LEDCONTROLLER_HPP
 
-struct RGB{
-    int r;
-    int g;
-    int b;
-};
-
 class LedController {
 public:
+    struct RGB{
+        int r;
+        int g;
+        int b;
+    };
+
+    LedController();
     RGB nextHueColor();
 private:
     float hue = 1.0;
