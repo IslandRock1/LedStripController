1f1e276f3ae6ec5d920859799650dfe16e650091
Øystein Bringsli <oysteinmb@gmail.com>
2024-01-27 19:23:32 +0100

    Example internetAccess

---
+++ include/exampleInternetAccess.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Øystein Bringsli.
+//
+
+#ifndef LEDSTRIPCONTROLLER_EXAMPLEINTERNETACCESS_HPP
+#define LEDSTRIPCONTROLLER_EXAMPLEINTERNETACCESS_HPP
+
+#include <string>
+
+std::string internetSSID = "internetSSID";
+std::string internetPassword = "internetPassword";
+
+#endif //LEDSTRIPCONTROLLER_EXAMPLEINTERNETACCESS_HPP
