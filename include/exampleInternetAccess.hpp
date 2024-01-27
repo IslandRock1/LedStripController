34ac879869836edb4c3b4680379bdb8350a3b2c4
Øystein Bringsli <oysteinmb@gmail.com>
2024-01-27 19:30:45 +0100

    Added wifi development

--- include/exampleInternetAccess.hpp
+++ include/exampleInternetAccess.hpp
@@ -5,9 +5,7 @@
 #ifndef LEDSTRIPCONTROLLER_EXAMPLEINTERNETACCESS_HPP
 #define LEDSTRIPCONTROLLER_EXAMPLEINTERNETACCESS_HPP
 
-#include <string>
-
-std::string internetSSID = "internetSSID";
-std::string internetPassword = "internetPassword";
+const char* internetSSID = "internetSSID";
+const char* internetPassword = "internetPassword";
 
 #endif //LEDSTRIPCONTROLLER_EXAMPLEINTERNETACCESS_HPP
